b6638b1ca0bf22ff779f4ed1c6ffb21ad6aaa3d6
Mark Virag <mark@viragconsulting.com>
2023-11-12 12:05:33 -0600

    Added a chart to explore cutoff levels

--- Copilot.py
+++ Copilot.py
@@ -1,14 +1,59 @@
 import pandas as pd
+import numpy as np
+import pandas as pd
+from pandas.api.types import is_numeric_dtype
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler 
+from sklearn.tree import DecisionTreeRegressor, export_text
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn import metrics
+import seaborn as sns
+sns.set_theme() 
+import tensorflow as tf
+from tensorflow import keras
+print(tf.__version__)
+train_data = []
+test_data = []
+train_y = []
+test_y = []
     
 '''read file from disk and create dataframe'''
 def read_file_df(filename):
     with open(filename, 'r') as f:
         return pd.read_csv(f)
     
-'''identify categorical and numerical variables'''
-def identify_vars(df):
+
     
+def create_neural_network(num_hidden_layers, output_dim):
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Input(shape=(31,)))
+    for _ in range(num_hidden_layers):
+        model.add(tf.keras.layers.Dense(256, activation='relu'))
+    model.add(tf.keras.layers.Dense(output_dim, activation='linear'))
+    model.compile(optimizer='adam', loss='mean_squared_error') 
+    return model
     
+def neural_net(n_hidden=3, n_neurons=256, learning_rate=0.005):
+    model = keras.models.Sequential()
+    model.add(keras.Input(train_data.shape[1],))
+    for layer in range(n_hidden):
+        model.add(keras.layers.Dense(n_neurons, activation='relu'))
+        model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Dense(1))
+    opt = keras.optimizers.Adam(learning_rate=learning_rate)
+    model.compile(optimizer=opt, loss='mean_squared_error')
+    model.summary()
+    history = model.fit(train_data, train_y, 
+        validation_data=(test_data, test_y), epochs=3, verbose=2)
+    return model, history
+
+
 
 
     
--- Loan_Default_Model.py
+++ Loan_Default_Model.py
@@ -11,7 +11,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler 
 from sklearn.tree import DecisionTreeRegressor, export_text
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
-from sklearn import metrics
+from sklearn.metrics import roc_curve, roc_auc_score, RocCurveDisplay
 import seaborn as sns
 sns.set_theme() 
 import tensorflow as tf
@@ -30,9 +30,9 @@ print(num_vars)
 
 train_set, test_set = train_test_split(train_df, test_size=0.20, random_state=42)
 train_temp = train_set.drop('Default', axis=1)
-train_y = train_set['Default'].copy() 
+y_train = train_set['Default'].copy() 
 test_temp = test_set.drop('Default', axis=1)
-test_y = test_set['Default'].copy() 
+y_test = test_set['Default'].copy() 
 
 num_pipeline = Pipeline([
     ('imputer', SimpleImputer(strategy = 'median')),
@@ -44,16 +44,16 @@ full_pipeline = ColumnTransformer([
     ('cat', OneHotEncoder(handle_unknown='ignore'), cat_vars) 
     ]) 
 
-train_data = full_pipeline.fit_transform(train_temp)
-test_data = full_pipeline.transform(test_temp) # Do NOT re-fit 
+X_train = full_pipeline.fit_transform(train_temp)
+X_test = full_pipeline.transform(test_temp) # Do NOT re-fit 
 feature_names = num_vars + full_pipeline.named_transformers_['cat'].get_feature_names_out(input_features=cat_vars).tolist()
 
 ### MODELS START HERE ###
 
-def result_roc(name, pred_y):
-    fpr, tpr, thresholds = metrics.roc_curve(test_y, pred_y)
-    roc_auc = metrics.auc(fpr, tpr)
-    display = metrics.RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc, estimator_name=name)
+def result_roc(name, y_pred):
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+    roc_auc = roc_auc_score(fpr, tpr)
+    display = RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc, estimator_name=name)
     display.plot()
     plt.show()
     
@@ -61,30 +61,18 @@ def neural_net(n_hidden=4, n_neurons=256, learning_rate=0.005):
     print("\n*** Hidden: "+ str(n_hidden))
     print("*** Neurons: " + str(n_neurons))
     print("*** Learning Rate: " + str(learning_rate))               
-    # train_data_dense = np.asarray(train_data) Not sparse 
-    # test_data_dense = np.asarray(test_data)
+    # X_train_dense = np.asarray(X_train) Not sparse 
+    # X_test_dense = np.asarray(X_test)
     model = keras.models.Sequential()
-    model.add(keras.Input(train_data.shape[1],))
+    model.add(keras.Input(X_train.shape[1],))
     for layer in range(n_hidden):
         model.add(keras.layers.Dense(n_neurons, activation='relu'))
-        model.add(keras.layers.BatchNormalization())
+        # model.add(keras.layers.BatchNormalization())
     model.add(keras.layers.Dense(1, activation='linear'))
     opt = keras.optimizers.Adam(learning_rate=learning_rate)
     model.compile(optimizer=opt, loss='mean_squared_error')
     model.summary()
-    history = model.fit(train_data, train_y, validation_data=(test_data, test_y), epochs=3, verbose=2)
-    return model, history
-
-def neural_net2(n_hidden=4, n_neurons=256):
-    model = keras.models.Sequential()
-    model.add(keras.Input(train_data.shape[1],))
-    for layer in range(n_hidden):
-        model.add(keras.layers.Dense(n_neurons, activation='relu'))
-        model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.Dense(1)) 
-    model.compile(optimizer='adam', loss='mean_squared_error')
-    model.summary()
-    history = model.fit(train_data, train_y, validation_data=(test_data, test_y), epochs=10, verbose=2)
+    history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3, verbose=2)
     return model, history
 
 def history_plot(history):
@@ -96,21 +84,60 @@ def history_plot(history):
     plt.legend(['train', 'val'], loc='upper right')
     plt.show()
 
+lin_reg = LinearRegression(n_jobs=-1).fit(X_train, y_train)  # 0.75 
+y_pred = lin_reg.predict(X_test)
+
 '''
-forest_class = RandomForestClassifier(max_depth=2, n_estimators=1000, criterion="entropy").fit(train_data, train_y) # 0.73
-pred_y = forest_class.predict_proba(test_data)[:, 1]
-result_roc("Forest Classifier", pred_y) 
+forest_class = RandomForestClassifier(max_depth=4, n_jobs=-1, verbose=1).fit(X_train, y_train) # 0.73
+y_pred = forest_class.predict_proba(X_test)[:, 1]
 
-lin_reg = LinearRegression().fit(train_data, train_y)  # 0.75 
-pred_y = lin_reg.predict(test_data)
-result_roc("Linear Regression", pred_y)
+forest_reg = RandomForestRegressor(max_depth=4).fit(X_train, y_train) # 0.72
+y_pred = forest_reg.predict(X_test)
+result_roc("Random Forest", y_pred)
 
-forest_reg = RandomForestRegressor(max_depth=4).fit(train_data, train_y) # 0.72
-pred_y = forest_reg.predict(test_data)
-result_roc("Random Forest", pred_y)
-'''
-
-nn_model, history = neural_net() # 0.76
-pred_y = nn_model.predict(test_data)
-result_roc("Neural Net", pred_y)
+nn_model, history = neural_net(3, 256) # 0.76
+y_pred = nn_model.predict(X_test)
 history_plot(history)
+'''
+
+# Plot the ROC curve
+fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+roc_auc = roc_auc_score(y_test, y_pred)
+plt.figure()
+plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC)')
+plt.legend(loc='lower right')
+plt.show()
+
+confusion = pd.DataFrame({"Proba":y_pred, "Actual":y_test})
+P = y_test.sum()
+N = y_test.count() - P
+specificity = []
+sensitivity = []
+cutoff = np.arange(.01, 1, .01)
+for t in cutoff:
+    confusion["Bool"] = (confusion["Proba"] >= t) * 1
+    confusion.eval("TP = ((Actual == 1) and (Bool == 1)) * 1", inplace=True)
+    confusion.eval("TN = ((Actual == 0) and (Bool == 0)) * 1", inplace=True)
+    TP = confusion["TP"].sum()
+    TN = confusion["TN"].sum()
+    TPR = TP / P if P > 0 else 1
+    TNR = TN / N if N > 0 else 1 
+    specificity.append(TNR)
+    sensitivity.append(TPR)
+
+plt.figure()
+plt.plot(cutoff, specificity, color='darkorange', lw=2, label='Specificity (TNR)')
+plt.plot(cutoff, sensitivity, color='navy', lw=2, label='Sensitivity (TPR)')
+plt.xlim([0.0, 1.0])
+plt.ylim([-0.05, 1.05])
+plt.xlabel('Threshold Value')
+plt.ylabel('True Pos/Neg Rate')
+plt.title('Truth Rates v. Proba Cutoff')
+plt.legend(loc='center right')
+plt.show()
